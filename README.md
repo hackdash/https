071c738d134349d5fb7ce0c593d383a2cd06d90b
Vezquex <admin@vezquex.com>
2014-05-18 23:14:34 -0700

    * https for root and API domains * redirect from www * not-found, home (placeholders)

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-hackdash
-========
+# hackdash
+web server with HTTPS
 
-TODO:
+Each users corresponds to a subdomain.
+Unfortunately, due to TLS, resource manipulation has to be done through one subdomain rather than at the actual locations.
+The API is a reserved subdomain, the same one as the TLS certificate.
+Its cookies are not accessible from other subdomains.
+2 domains are not necessary if you avoid using cookies on the top level.
+
+
+## Setup
+- Generate a TLS certificate and key (subdomain for the API)
+- Development: `/etc/hosts` Add subdomains as needed.
+```
+127.0.0.1       localhost
+127.0.0.1       api.localhost
+127.0.0.1       x.localhost
+```
+- Production: DNS records: domain, wildcard subdomain
+- Edit `config.json`
+- Terminal:
+-- `cd` to this directory
+-- `nodejs ./server.js`
+-- For ports below 1024, prefix with `sudo `. (You should forward higher ports to avoid.)
+
+## To Do
+- forward ports
+- multiple domains
+- registration
 - auth
 - lists
 - posts
--- server.js
+++ server.js
@@ -1,10 +1,73 @@
 var config = require('./config.json');
+var ensure_https_handle = require('ensure-https')
+	.handle
+	.bind({sslPort: config.https.port});
 var fs = require('fs');
+var rd = require('./lib/http/redirect-domain.js');
 
-var respond = function (req, res) {
-	res.writeHead(200);
-	res.end("https response\n");
+var root = 'http://'+config.domain.root+':'+config.http.port;
+var subdomains = config.domain.root.split('.');
+var request_domain = function(req){
+	return req.headers.host.split(':')[0];
 }
+
+var respond = {};
+
+respond.http = function(req, res){
+	var domain = request_domain(req);
+	var req_subdomains = domain.split('.').slice(0, -1 * subdomains.length);
+	var subdomain = req_subdomains[0];
+
+	// https for root and api domains
+	if(!subdomain || (config.domain.api === domain)){
+		return ensure_https_handle(req, res);
+	}
+
+	var www = rd.redirectWww(req, res);
+	if(www) return;
+
+	if(subdomain){
+		return respond.subdomain_missing(req, res);
+	}
+	respond.notfound(req, res);
+}
+
+respond.https = function(req, res){
+	var www = rd.redirectWww(req, res, true);
+	if(www) return;
+
+	var response_type = (req.url === '/') ? 'home' : 'notfound';
+	respond[response_type](req, res);
+}
+
+respond.notfound = function (req, res){
+	res.writeHead(404);
+	res.end("Not Found");
+	console.log('not found: ', req.headers.host + req.url);
+}
+
+respond.home = function (req, res){
+	res.writeHead(200);
+	res.end("hackdash");
+}
+
+respond.subdomain_missing = function (req, res){
+	res.writeHead(404);
+	res.end(
+		'<a href="'+root+'/?domain='+request_domain(req)+'">'
+		+  'Subdomain available.'
+		+'</a>'
+	);
+	// res.writeHead(302, 'Subdomain unregistered.');
+	// res.setHeader('Location', root+'/?domain='+request_domain(req));
+	// res.end();
+}
+
+require('http')
+	.createServer(respond.http)
+	.listen(config.http.port);
+console.log('http://'+config.domain.root+':'+config.http.port+'/');
+console.log('http://x.'+config.domain.root+':'+config.http.port+'/');
 
 require('https')
 	.createServer(
@@ -12,12 +75,8 @@ require('https')
 			cert: fs.readFileSync(config.https.cert),
 			key: fs.readFileSync(config.https.key)
 		},
-		respond
+		respond.https
 	)
 	.listen(config.https.port);
-
-require('ensure-https')
-	.createServer({
-		sslHost: config.https.port
-	})
-	.listen(config.http);
+console.log('https://api.'+config.domain.root+':'+config.https.port+'/');
+console.log('https://'+config.domain.root+':'+config.https.port+'/');
